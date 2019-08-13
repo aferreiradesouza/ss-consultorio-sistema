cf7a405cb001e104b7351cfc1531d72ce3965eba
Arthur Ferreira <arthur.ferreira@ed.company>
2019-08-13 19:51:48 -0300

    criacao calendario

--- src/app/shared/components/calendario/calendario.component.ts
+++ src/app/shared/components/calendario/calendario.component.ts
@@ -10,13 +10,34 @@ import { CalendarioService } from './calendario.service';
 })
 
 export class CalendarioComponent implements OnInit {
+
   constructor(public calendarioService: CalendarioService) { }
 
   ngOnInit() {
-
+    this.obterHoras();
   }
 
-  get obterArray() {
-    return this.calendarioService.criarArray(7);
+  obterArray(num) {
+    return this.calendarioService.criarArray(num);
+  }
+
+  obterHoras() {
+    const horasLength = 24;
+    const horarios = this.calendarioService.criarArray(horasLength).map((e, index) => index++);
+    return horarios;
+  }
+
+  clickHora(dia, minuto) {
+    const decimal = (minuto * 24) / this.obterArray(24 * 4).length;
+    let decimalTime = decimal * 60 * 60;
+    const hours = Math.floor((decimalTime / (60 * 60)));
+    decimalTime = decimalTime - (hours * 60 * 60);
+    const minutes = Math.floor((decimalTime / 60));
+    console.log(`${dia}, ${hours.toString().length === 1 ? `${0}${hours}` : hours}:${minutes}`);
+  }
+
+  pegarHora(hora: number) {
+    const horaGrid = hora.toString();
+    return horaGrid.length === 1 ? `0${horaGrid}:00` : `${horaGrid}:00`;
   }
 }
